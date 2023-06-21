f2087236fe097ecc1b6db710e48ec172f249d670
surajsinghit21 <surajsinghit21@gmail.com>
2023-06-21 18:55:31 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>{
+
+   const fetchData = () => {fetch('https://test-static-webapp.azurewebsites.net/api/Function1').then(res =>res.json()).then().catch();};
+
+   return(
+    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+   )
+  
+   }
+
+export default App;
